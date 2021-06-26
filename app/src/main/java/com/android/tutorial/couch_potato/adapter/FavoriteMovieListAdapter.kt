9dafcd316d8cba9f3826e2278d67bd2220c879b2
eieikyaw <eieikyaw@advent-soft.com>
2021-06-26 14:27:55 +0630

    save favorite movie list in history

---
+++ app/src/main/java/com/android/tutorial/couch_potato/adapter/FavoriteMovieListAdapter.kt
@@ -0,0 +1,78 @@
+package com.android.tutorial.couch_potato.adapter
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.android.tutorial.couch_potato.R
+import com.android.tutorial.couch_potato.activity.MovieDetailActivity
+import com.android.tutorial.couch_potato.model.Movie
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_movie_detail.view.ivMoviePoster
+import kotlinx.android.synthetic.main.item_movie_detail.view.tvMovieTitle
+import kotlinx.android.synthetic.main.item_movie_list.view.*
+
+class FavoriteMovieListAdapter() :
+    RecyclerView.Adapter<FavoriteMovieListAdapter.MyViewHolder>() {
+
+    private val movieList = mutableListOf<Movie>()
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_history_movie, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val movie = movieList[position]
+
+        holder.itemView.apply {
+            Glide.with(context)
+                .load(movie.posterImg)
+                .error(R.drawable.sample_poster)
+                .into(ivMoviePoster)
+            tvMovieTitle.text = movie.title
+        }
+
+        holder.itemView.setOnClickListener {
+            val context = holder.itemView.context
+            val intent = Intent(context, MovieDetailActivity::class.java)
+
+            intent.putExtra("moviePoster", movie.posterImg)
+            intent.putExtra("movieTitle", movie.title)
+            intent.putExtra("releasedDate", movie.releasedYear)
+            intent.putExtra("description", movie.plot)
+            intent.putExtra("imdbRating", movie.imdbRating)
+            intent.putExtra("imdbVotes", movie.imdbVotes)
+            intent.putExtra("awards", movie.awards)
+            intent.putExtra("actors", movie.actors)
+            intent.putExtra("genre", movie.category)
+
+            intent.putExtra("imdbId", movie.imdbID)
+            intent.putExtra("isFavorite", holder.itemView.ivFavorite.isSelected)
+            intent.putExtra("isBookmark", holder.itemView.ivBookmark.isSelected)
+            context.startActivity(intent)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return movieList.size
+    }
+
+    fun setNewDataList(list: List<Movie>) {
+        movieList.clear()
+        movieList.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    fun setNewData(movie: Movie) {
+        movieList.add(movie)
+        notifyItemInserted(movieList.size - 1)
+    }
+
+}
--- app/src/main/java/com/android/tutorial/couch_potato/fragment/HistoryFavoriteFragment.kt
+++ app/src/main/java/com/android/tutorial/couch_potato/fragment/HistoryFavoriteFragment.kt
@@ -1,18 +1,76 @@
 package com.android.tutorial.couch_potato.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.android.tutorial.couch_potato.R
+import com.android.tutorial.couch_potato.adapter.FavoriteMovieListAdapter
+import com.android.tutorial.couch_potato.model.Movie
+import com.android.tutorial.couch_potato.rest.RestClient
+import com.android.tutorial.couch_potato.viewmodel.MovieDetailViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class HistoryFavoriteFragment: Fragment() {
+
+    private lateinit var viewModel: MovieDetailViewModel
+    private lateinit var adapter: FavoriteMovieListAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.frag_history_favorite, container, false)
+        val view = inflater.inflate(R.layout.frag_history_favorite, container, false)
+        adapter = FavoriteMovieListAdapter()
+        viewModel = MovieDetailViewModel()
+        val rvMovies: RecyclerView = view.findViewById(R.id.rvFavoriteMovies)
+        getFavMovies()
+        rvMovies.adapter = adapter
+        rvMovies.layoutManager =
+            LinearLayoutManager(view.context, LinearLayoutManager.VERTICAL, false)
+        return view
+    }
+
+    private fun getFavMovies() {
+        val collection = FirebaseFirestore.getInstance().collection("favorite-movies")
+        collection.get().addOnCompleteListener {
+            if (it.isSuccessful) {
+                for (document in it.result!!) {
+                    if (document.data.getValue("isFavorite") == true) {
+                        getMovie(document.data.getValue("imdbId").toString())
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getMovie(imdbId: String) {
+        RestClient.getApiService()
+            .getById(imdbId, "full")
+            .enqueue(object : Callback<Movie> {
+                override fun onResponse(
+                    call: Call<Movie>,
+                    response: Response<Movie>
+                ) {
+                    if (response.isSuccessful) {
+                        response.body()?.let { movie ->
+                            adapter.setNewData(movie)
+                            Log.d("favresponse", "movie............." + movie.title)
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<Movie>, t: Throwable) {
+                    Log.d("favresponse", "......fail")
+                }
+            })
     }
 }
